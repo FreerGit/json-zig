1fdc2b454eef4f58bc8bc20420d03928dd315c3d
FreerGit <fredrik-98@live.se>
2024-03-31 23:18:38 +0200

    zig init

---
+++ build.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Make the lib discoverable by zig pkg manager (build.zig.zon).
+    _ = b.addModule("json-zig", .{ .root_source_file = .{ .path = "src/mod.zig" } });
+
+    // Setup tests
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
